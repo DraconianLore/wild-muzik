4f2236aff4114dd8e45da475057fcfe0f22aab4e
Steven <steven.wing82@gmail.com>
2018-03-30 09:44:38 +1100

    fixed monkey

--- WildMuzik/Assets/Scripts/hideanimals.cs
+++ WildMuzik/Assets/Scripts/hideanimals.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class hideanimals : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public SpriteRenderer hider;
+    // Use this for initialization
+    void Start()
+    {
+        
+       
+
+    }
+    public void Hidethem()
+    {
+        hider = GetComponent<SpriteRenderer>();
+        hider.enabled = true;
+        StartCoroutine(countdown());
+    }
+    IEnumerator countdown()
+    {
+
+        yield return new WaitForSeconds(3);
+        hider.enabled = false;
+
+    }
+
+// Update is called once per frame
+void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
